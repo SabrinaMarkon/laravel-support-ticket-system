ec9e997d7d0321605e2292a50a251dcda73c7825
Sabrina Markon <phpsitescripts@outlook.com>
2018-07-11 01:11:14 -0600

    Install Laravel and basic website template

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('master')
+@section('title', 'About')
+
+@section('content')
+        <div class="title m-b-md text-center">
+            About Page
+        </div>
+        <div class="quote text-center">Magic is essentially the higher understanding of nature</div>
+@endsection
