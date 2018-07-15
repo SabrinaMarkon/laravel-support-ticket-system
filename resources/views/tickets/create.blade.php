a127445fdda886be9c4b8d1116f6d1af470d1134
Sabrina Markon <phpsitescripts@outlook.com>
2018-07-14 23:57:15 -0600

    Add csrf_token to contact form as well as field names

--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -4,19 +4,26 @@
 @section('content')
 <div class="container col-md-8 col-md-offset-2">
         <div class="well well bs-component">
-            <form class="form-horizontal">
+            <form class="form-horizontal" method="post">
+            <input type="hidden" name="_token" value="{!! csrf_token() !!}">
                 <fieldset>
                     <legend>Submit a new ticket</legend>
                     <div class="form-group">
                         <label for="title" class="col-lg-2 control-label">Title</label>
                         <div class="col-lg-10">
-                            <input type="text" class="form-control" id="title" placeholder="Title">
+                            <input type="text" class="form-control" id="title"
+                            name="title"
+                            placeholder="Title">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="content" class="col-lg-2 control-label">Content</label>
                         <div class="col-lg-10">
-                            <textarea class="form-control" rows="3" id="content"></textarea>
+                            <textarea class="form-control"
+                            rows="3"
+                            id="content"
+                            name="content">
+                            </textarea>
                             <span class="help-block">Feel free to ask us any question.</span>
                         </div>
                     </div>
